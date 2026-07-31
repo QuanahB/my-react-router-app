70e15fa39033ad2495281a3540f298546ee26dd8
quanbenn <77028868+quanbenn@users.noreply.github.com>
2026-07-31 17:05:40 -0400

    Base level Logo Component(No Image Yet)

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,10 +1,36 @@
 import LogoImage from "~/assets/birdLogo.avif"
 import type { ImgHTMLAttributes,ReactNode } from "react"
+export type LogoAlternate = "main" | "alternate"
 
 export type LogoProps = {
     title?: string;
     className?: string;
+    alternate?: LogoAlternate;
 
 } & Omit<ImgHTMLAttributes<HTMLImageElement>,"className">;
 
-export function Logo({})
+//Need to pass the image into the class and then work on variatons.
+const alternateClasses: Record<LogoAlternate, string> = {
+  main:["bg-stone-900 text-stone-50"].join(" "),
+  alternate:["bg-transparent text-stone-900"].join(" ")
+};
+
+export function Logo({
+    title = "Website Logo",
+    className = "",
+    alternate = "main",
+    ...rest
+}: LogoProps){
+    return(
+        <img
+        className={[
+            className,
+            alternateClasses[alternate]
+        ]
+            .filter(Boolean)
+            .join(" ")}
+        {...rest}
+        >
+        </img>
+    );
+}
